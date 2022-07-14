842beee60564973496e4f2c942c7f5e3041ec546
Roberto Allende <roberto@atomic.io>
2022-07-15 10:09:13 +1200

    Add boilerplate code for Flutter SDK v0.2.0

---
+++ android/app/src/main/kotlin/io/atomic/flutter_boilerplate/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.atomic.flutter_boilerplate
+
+import io.atomic.atomic_sdk_flutter.AACFlutterActivity
+
+class MainActivity: AACFlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:atomic_sdk_flutter/atomic_session.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Atomic Boilerplate',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Atomic Boilerplate'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Atomic Demo',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
