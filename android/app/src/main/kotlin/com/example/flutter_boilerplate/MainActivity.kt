cff372c0403b74f4929d6dd52f3ffc5905a31ac5
Will <will.bryant.will@gmail.com>
2023-11-20 16:15:33 +1300

    refactoring and fixes (#3)
    - Updated pod stuff  
        The boilerplate app didn't have the line "pod 'AtomicSDK', '1.1.8'" at the bottom of Podfile in the ios folder. I had to add that then run pod repo update then pod install in a terminal from the ios folder.
    - Updated to 1.0.0
    - AACDelegate  
        In the boilerplate app, AACStreamController used a parameter called sessionDelegate, but that doesn't exist. So I had to call AACSession.setDelegate(MySessionDeletegate()) in the onLogin() method.
    - In atomic_configuration.dart the line config.enabledUiElements &= ~AACUIElement.cardListHeader disables the header, not enables it like it said in the comment above it. I had to comment it out it for the header to show up. Same for the AACUIElement.cardListToast.
    - Swapped out future builder approach for an init state Future approach.  
        I don't think the use of a FutureBuilder is a good idea in the build method here because it means it runs the onLogin() method every time the app rebuilds. So if the setState method is called it will reload. This can be bad because the cards take more than a second to load and if the state is changed frequently the card will have to login each time, making it always loading. I rewrote it so that login only happens once, in the initState(), otherwise it might confuse or annoy users trying to fix it.

--- android/app/src/main/kotlin/com/example/flutter_boilerplate/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_boilerplate/MainActivity.kt
@@ -1,5 +1,6 @@
-package io.atomic.flutter_boilerplate
+package com.example.flutter_boilerplate
 
+//import io.flutter.embedding.android.FlutterActivity
 import io.atomic.atomic_sdk_flutter.AACFlutterActivity
 
 class MainActivity: AACFlutterActivity() {
